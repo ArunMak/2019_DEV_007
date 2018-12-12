1f0bd7a7661b4661e886857bbe8d22cfca6a6f7e
ArunKumar <arunmakk@gmail.com>
2018-12-12 21:42:14 +0530

    Created UIColor extension . Change button color when selected implemented and clear field values when switching between input views

--- BerlinClock/BerlinClock/Controller/BerlinClockVC.swift
+++ BerlinClock/BerlinClock/Controller/BerlinClockVC.swift
@@ -13,19 +13,66 @@ class BerlinClockVC: UIViewController {
     @IBOutlet var selectOptionButton: [UIButton]!
     @IBOutlet var selectBerlinOptionButton: [UIButton]!
     
+    // MARK: Instance variable
+    private var datePicker : UIDatePicker!
+   
     // MARK: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         convertButton.isHidden = true
+        addPicker()
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Add Picker
+    fileprivate func addPicker(){
+        self.datePicker = UIDatePicker(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        self.datePicker.datePickerMode = UIDatePickerMode.time
+        self.datePicker.locale = Locale(identifier: "en_GB")
+        digitalTextField.inputView = self.datePicker
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(self.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        digitalTextField.inputAccessoryView = toolBar
+    }
+    @objc func doneClick () {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .long
+        dateFormatter.dateFormat = "HH:mm:ss"
+        digitalTextField.text = dateFormatter.string(from: datePicker.date)
+        digitalTextField.resignFirstResponder()
+    }
+    @objc func cancelClick() {
+        digitalTextField.resignFirstResponder()
+    }
+    
     // MARK: Convert Option selected action.
     //Used to display from berlin to digital or digital to berlin view based on selection
     @IBAction func changeConverterButtonAction(_ sender: UIButton) {
+        clearField()
+        for button in selectOptionButton {
+            if button.tag == sender.tag{
+                button.backgroundColor = UIColor(red: 68, green: 143, blue: 103)
+            } else{
+                button.backgroundColor = UIColor(red: 175, green: 44, blue: 37)
+            }
+        }
         if sender.tag == 2 {
             convertButton.isHidden = true
             digitalView.isHidden = false
@@ -38,8 +85,23 @@ class BerlinClockVC: UIViewController {
             
         }
     }
+    func clearField(){
+        secondsTextField.text = ""
+        minuteLineOneTextField.text = ""
+        minuteLineTwoTextField.text = ""
+        hourLineOneTextField.text = ""
+        hourLineTwoTextField.text = ""
+        digitalTextField.text = ""
+    }
     //Used to select the option to display ouput from digital to berlin format in different options  like seconds in berlin ,or full berlin time
     @IBAction func convertDigitalToBerlin(_ sender: UIButton) {
+        for button in selectBerlinOptionButton {
+            if button.tag == sender.tag {
+                button.backgroundColor = UIColor(red: 68, green: 143, blue: 103)
+            }else{
+                button.backgroundColor = UIColor(red: 175, green: 44, blue: 37)
+            }
+        }
     }
    
     // MARK: Convert button action
